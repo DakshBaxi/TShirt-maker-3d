fa3e4ff2320891e5ee55657da05ca5ec0354ae89
DakshBaxi <111555273+DakshBaxi@users.noreply.github.com>
2024-01-09 19:06:00 +0530

    Add files via upload

---
+++ src/components/index.js
@@ -0,0 +1,12 @@
+import AIPicker from "./Aipicker";
+import ColorPicker from "./ColorPicker";
+import CustomButton from "./CustomButton";
+import FilePicker from "./FilePicker";
+import Tab from "./Tab"
+export {
+    CustomButton,
+    AIPicker,
+    ColorPicker,
+    FilePicker,
+    Tab    
+};
